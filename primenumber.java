1c38aa35fb9bc2dcfeee9f616ac407e7dd292d6d
Sakthi2410 <104966660+Sakthi2410@users.noreply.github.com>
2022-05-05 11:42:46 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,23 @@
+package week1.day3;
+
+public class primenumber {
+
+	public static void main(String[] args) {
+		int n=13,i;
+		int count=0;
+		for( i=1; i<=n; i++) {
+		if(n%i==0) {
+			count++;
+		}
+		}
+		if(count==2)
+		{
+			System.out.println(" prime number");
+		}
+		else
+		{
+			System.out.println("Not a Prime number");
+		}
+	}
+
+}
